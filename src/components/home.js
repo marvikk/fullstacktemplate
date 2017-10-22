e68efd90dd974d1f877918dfaceb88803552d781
marvikk <russzkij@yahoo.com>
2017-10-22 04:08:53 +0300

    sync client server

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,23 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
-export default class Home extends Component{
-  render(){
-    return <div>Home</div>
+export default class Home extends Component {
+  componentWillMount() {
+    axios
+      .post("http://localhost:3001/destination", {
+        name: "cuba" + Math.random()
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    axios
+      .get("http://localhost:3001/alldestinations")
+      .then(res => console.log(res));
+  }
+  render() {
+    return <div>Home!</div>;
   }
 }
